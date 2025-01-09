c7b0908aba94a446dbaf6c9602a38322bc9a2656
Johnathon Reidel <jreidel8@gmail.com>
2025-01-08 20:58:58 -0800

    Fixed some arm code

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Filesystem;
+import frc.robot.tagalong.ArmParser;
 import frc.robot.tagalong.PivotAugment;
 import frc.robot.tagalong.PivotParser;
 import frc.robot.tagalong.TagalongPivot;
@@ -8,6 +9,10 @@ import frc.robot.tagalong.TagalongSubsystemBase;
 
 public class ArmSubsystem extends TagalongSubsystemBase implements PivotAugment{
     private final TagalongPivot arm;
+        private static ArmSubsystem armSubsystem;
+        public final ArmParser armParser;
+
+
 
 
     public ArmSubsystem(String filePath) {
@@ -15,15 +20,26 @@ public class ArmSubsystem extends TagalongSubsystemBase implements PivotAugment{
     }
 
 
-    public ArmSubsystem(){
-        super(ArmParser arm);
+    public ArmSubsystem(ArmParser parser){
+        super(parser);
+        armParser = parser;
+        arm = new TagalongPivot(armParser.pivotParser);
+
+        
     }
+
+    public static ArmSubsystem getInstance() {
+        if (armSubsystem == null) {
+            armSubsystem = new ArmSubsystem("configs/subsystems/armConf");
+        }
+        return armSubsystem;
+    }
+
 
 
     @Override
     public TagalongPivot getPivot() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPivot'");
+        return arm;
     }
 
 
