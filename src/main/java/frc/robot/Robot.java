d139c29b462ac3d872dec797e20a61a810c84c59
RohanP321 <120353864+Rohan3476@users.noreply.github.com>
2025-01-11 17:02:33 -0800

    fixed errors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ import com.ctre.phoenix6.SignalLogger;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.IntakeSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -22,7 +21,7 @@ public class Robot extends TimedRobot {
 
   private RobotContainer robotContainer;
 
-  private IntakeSubsystem exampleSubsystem = IntakeSubsystem.getInstance();
+//  private IntakeSubsystem exampleSubsystem = IntakeSubsystem.getInstance();
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -33,7 +32,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
 
-    exampleSubsystem.init();
+//    exampleSubsystem.init();
 
     robotContainer = new RobotContainer();
   }
--- src/main/java/frc/robot/subsystems/testing/LinearTestSubsystem.java
+++ src/main/java/frc/robot/subsystems/testing/LinearTestSubsystem.java
@@ -1,18 +1,18 @@
 package frc.robot.subsystems.testing;
 
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import frc.robot.subsystems.template.AbstractSubsystem;
+import frc.robot.subsystems.template.TemplateSubsystem;
 import frc.robot.utility.FeedForward;
 import frc.robot.utility.PID;
 import frc.robot.utility.Type;
 
-public class LinearTestSubsystem extends AbstractSubsystem {
+public class LinearTestSubsystem extends TemplateSubsystem {
     public LinearTestSubsystem() {
-        super(Type.LINEAR, new int[]{22},
+        super(Type.LINEAR, 22,
                 new TrapezoidProfile.Constraints(3, 7.5),
                 new PID(2, 0, 0), new FeedForward(.4, .205, 8.3333333333333333333333333333333),
                 .015, .015, new double[][]{{48, 14}, {48, 24}});
-        configureMotors(false, true, new double[][]{{80, 80}});
+        configureMotor(false, true, 80,80);
         configureFollowerMotor(23);
         configureLinearMech(1.273, 0, 100);
     }
