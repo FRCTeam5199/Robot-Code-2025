ab93ff9d8dbdbe985b4ba3183f00f3ae1ef9aeaf
ajiteshsingh123 <ajiteshsingh353@gmail.com>
2025-01-13 18:48:01 -0800

    Added names to commands

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,13 +4,23 @@
 
 package frc.robot.commands;
 
+import com.fasterxml.jackson.databind.util.Named;
 import com.pathplanner.lib.auto.AutoBuilder;
 
+import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public final class Autos {
-  private Autos() {}
+  private Autos() {
+    NamedCommands.registerCommand("intake", new SequentialCommandGroup(null));
+    NamedCommands.registerCommand("outtake", new SequentialCommandGroup(null));
+    NamedCommands.registerCommand("humanPlayerStation", new SequentialCommandGroup(null));
+    NamedCommands.registerCommand("reef1", new SequentialCommandGroup(null));
+    NamedCommands.registerCommand("reef2", new SequentialCommandGroup(null));
+    NamedCommands.registerCommand("reef3", new SequentialCommandGroup(null));
+  }
   
   private static SendableChooser<Command> autoChooser;
 
@@ -21,4 +31,6 @@ public final class Autos {
     if (autoChooser == null) autoChooser = AutoBuilder.buildAutoChooser();
     return autoChooser;
   }
+
+
 }
