983fd543d311147bbd6da0d18fa307ed24fabfd4
Johnathon Reidel <jreidel8@gmail.com>
2025-01-21 18:23:33 -0800

    j,n

--- src/main/java/frc/robot/commands/ScoreCommands.java
+++ src/main/java/frc/robot/commands/ScoreCommands.java
@@ -15,44 +15,38 @@ public class ScoreCommands {
             
     public static Command scoreHP(){
         return new SequentialCommandGroup(
-            arm.setHP(),
-            elevator.setHP()
+   
         );
     }
 
     public static Command scoreL1(){
         return new SequentialCommandGroup(
-            arm.setL1(),
-            elevator.setL1()
+  
         );
     }
 
     public static Command scoreL2(){
         return new SequentialCommandGroup(
-            arm.setL2(),
-            elevator.setL2()
+ 
         );
     }
 
     public static Command scoreL3(){
         return new SequentialCommandGroup(
-            arm.setL3(),
-            elevator.setL3()
+
         );
     }
 
     
     public static Command scoreL4(){
         return new SequentialCommandGroup(
-            arm.setL4(),
-            elevator.setL4()
+        
         );
     }
 
     public static Command dunk(){
         return new SequentialCommandGroup(
-            arm.setDunk(),
-            wrist.setDunk()
+       
         );
     }
     
