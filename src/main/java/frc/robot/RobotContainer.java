9575645e3b7ea0a06eebf2dd848e859233073dd2
Robert Goll <68445266+robert-goll@users.noreply.github.com>
2025-01-26 20:43:46 -0800

    lil update wit da conditionalcommand

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.ScoreCommands;
@@ -91,48 +92,7 @@ public class RobotContainer {
             drivetrain.resetPose(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
 
         }
-
-        //     commandXboxController.a().onTrue(new InstantCommand(() -> pivotTestSubsystem.setPosition(10)))
-        //             .onFalse(new InstantCommand(() -> pivotTestSubsystem.setPosition(0)));
-
-//               commandXboxController.povLeft().onTrue(new InstantCommand(() -> elevator.setVoltage(1.175)));
-        //     commandXboxController.povLeft().onTrue(armSubsystem.setGround());
-        //     commandXboxController.povRight().onTrue(armSubsystem.setL1());
-        //     commandXboxController.povDown().onTrue(armSubsystem.setL3());
-        //  commandXboxController.povDown().onTrue(new InstantCommand(()-> linearTestSubsystem.setPosition(1)));
-
-
-        // commandXboxController.leftBumper().onTrue(Commands.runOnce(SignalLogger::start).alongWith(new PrintCommand("Start")));
-        // commandXboxController.rightBumper().onTrue(Commands.runOnce(SignalLogger::stop).alongWith(new PrintCommand("End")));
-
-        // commandXboxController.povLeft().onTrue(drivetrain.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-        // commandXboxController.povRight().onTrue(drivetrain.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-        // commandXboxController.povUp().onTrue(drivetrain.sysIdDynamic(SysIdRoutine.Direction.kForward));
-        // commandXboxController.povDown().onTrue(drivetrain.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-
-//        commandXboxController.povUp().onTrue(armSubsystem.setGround());
-//        commandXboxController.povDown().onTrue(armSubsystem.setL1());
-//        commandXboxController.povLeft().onTrue(armSubsystem.setL2());
-//        commandXboxController.povRight().onTrue(armSubsystem.setL3());
         
-
-//        commandXboxController.b().onTrue((new VelocityCommand());
-//        commandXboxController.x().onTrue(new InstantCommand(() -> wrist.setPercent(.1))).onFalse(new InstantCommand(() -> wrist.setPercent(0.)));
-
-        // commandXboxController.povUp().onTrue(elevatorSubsystem.setBase());
-        // commandXboxController.povDown().onTrue(elevatorSubsystem.setL1());
-        // commandXboxController.povLeft().onTrue(elevatorSubsystem.setL2());
-        // commandXboxController.povRight().onTrue(elevatorSubsystem.setL3());
-//         commandXboxController.b().onTrue(new PositionCommand(wrist, 30)).onFalse(new PositionCommand(wrist, 0));
-
-        //arm = 77, elevator = 0.13, wrist = 89, hp
-        //arm = , elevator = , wrist = , l1
-        //arm = 93, elevator = .067, wrist = 18.7, l2
-        //arm = 97.2, elevator = .445, wrist = 20.0, l3
-        //arm = 99.5, elevator = .950, wrist = , 27.76 l4
-
-        //arm = 77.03, elevator = .314, wrist = 95.3   HP
-
         commandXboxController.rightBumper().onTrue(new InstantCommand(() -> System.out.println("Arm Degrees: " + armSubsystem.getDegrees()))
                 .andThen(new InstantCommand(() -> System.out.println("Elevator Meters: " + elevatorSubsystem.getMechM())))
                 .andThen(new InstantCommand(() -> System.out.println("Wrist Degrees: " + wrist.getDegrees()))));
@@ -140,72 +100,22 @@ public class RobotContainer {
 
         commandXboxController.button(9).onTrue(new InstantCommand(() -> algaeControls = false));
         commandXboxController.button(10).onTrue(new InstantCommand(() -> algaeControls = true));
-        commandXboxController.a().onTrue(ScoreCommands.intakeHP());
-        commandXboxController.b().onTrue(new InstantCommand(() -> {if(algaeControls){
-                ScoreCommands.algaeL1();
-                }else{
-                       ScoreCommands.scoreL2();         
-                }
-                }
-        ));
-        commandXboxController.x().onTrue(new InstantCommand(() -> {if(algaeControls){
-                ScoreCommands.algaeL2();
-                }else{
-                        ScoreCommands.scoreL3();
-                }
-                }
-        ));
-        commandXboxController.y().onTrue(ScoreCommands.scoreL4());
-        commandXboxController.leftBumper().onTrue(new InstantCommand(() -> {if(algaeControls){
-                ScoreCommands.algaeStable();
-                }else{
-                        ScoreCommands.stable();
-                }
-                }
-        ));
 
-        // commandXboxController.a().onTrue(ScoreCommands.intakeHP());
-        // commandXboxController.b().onTrue(ScoreCommands.scoreL2());
-        // commandXboxController.x().onTrue(ScoreCommands.scoreL3());
-        // commandXboxController.y().onTrue(ScoreCommands.scoreL4());
-        // commandXboxController.leftBumper().onTrue(ScoreCommands.stable());
+        commandXboxController.a().onTrue(ScoreCommands.intakeHP());
+        commandXboxController.b().onTrue(new ConditionalCommand(ScoreCommands.algaeL1(), ScoreCommands.scoreL2(), () -> algaeControls));
+        commandXboxController.x().onTrue(new ConditionalCommand(ScoreCommands.algaeL2(), ScoreCommands.scoreL3(), () -> algaeControls));
+        commandXboxController.y().onTrue(ScoreCommands.scoreL4());
+        
+        commandXboxController.leftBumper().onTrue(new ConditionalCommand(ScoreCommands.algaeStable(), ScoreCommands.stable(), () -> algaeControls));
 
         commandXboxController.leftTrigger().onTrue(new InstantCommand(() -> intakeSubsystem.setPercent(60)))
                 .onFalse(new InstantCommand(() -> intakeSubsystem.setVoltage(0)));
         commandXboxController.rightTrigger().onTrue(new InstantCommand(() -> intakeSubsystem.setPercent(-80)))
                 .onFalse(new InstantCommand(() -> intakeSubsystem.setVoltage(0)));
 
-        // commandXboxController.rightBumper().onTrue(new InstantCommand(() -> System.out.println("Elevator: " + elevatorSubsystem.getMechM())));
-
         commandXboxController.povUp().onTrue(new InstantCommand(() -> climber.setPercent(0.3))).onFalse(new InstantCommand(() -> climber.setPercent(0)));
         commandXboxController.povDown().onTrue(new InstantCommand(() -> climber.setPercent(-0.3))).onFalse(new InstantCommand(() -> climber.setPercent(0)));
 
-        // commandXboxController.povDown().onTrue(ScoreCommands.scoreHP());
-        // commandXboxController.povUp().onTrue(ScoreCommands.scoreL1());
-
-        // commandXboxController.povLeft().onTrue(ScoreCommands.scoreL2());
-        // commandXboxController.povRight().onTrue(ScoreCommands.scoreL3());
-        // commandXboxController.a().onTrue(ScoreCommands.scoreL4());
-        // commandXboxController.rightBumper().onTrue(ScoreCommands.dunk());
-
-
-
-    //    commandXboxController.povDown().onTrue(climber.moveDOWN()).onFalse(new InstantCommand(()->climber.setPercent(0)));
-
-
-        //   commandXboxController.leftBumper().toggleOnTrue(arm.)
-        /*
-         * Joystick Y = quasistatic forward
-         * Joystick A = quasistatic reverse
-         * Joystick B = dynamic forward
-         * Joystick X = dyanmic reverse
-         */
-
-
-//        commandXboxController.y().whileTrue(elevatorSubsystem.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-//        commandXboxController.a().whileTrue(elevatorSubsystem.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-//        commandXboxController.b().whileTrue(elevatorSubsystem.sysIdDynamic(SysIdRoutine.Direction.kForward));
-//        commandXboxController.x().whileTrue(elevatorSubsystem.sysIdDynamic(SysIdRoutine.Direction.kReverse));
         drivetrain.registerTelemetry(logger::telemeterize);
     }
 
