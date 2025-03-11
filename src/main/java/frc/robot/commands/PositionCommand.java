4ebb6b9ae0ebf44fa43ac0b0125eb2bf65d30e9c
leonardosent <leonardosent@gmail.com>
2025-03-10 18:59:07 -0700

    reverted revert

--- src/main/java/frc/robot/commands/PositionCommand.java
+++ src/main/java/frc/robot/commands/PositionCommand.java
@@ -1,6 +1,7 @@
-package frc.robot.subsystems.template;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.template.TemplateSubsystem;
 
 public class PositionCommand extends Command {
     private double vel;
