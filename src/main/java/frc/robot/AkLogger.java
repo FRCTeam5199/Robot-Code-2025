d3d4a51a237711f5ca745c19c2675dc304827d30
James Y. Wilson <jywilson@ieee.org>
2025-10-01 17:23:15 -0700

    WIP: Adding support for the AdvantageKit LoggedRobot class, contained in a new hybrid TimedLoggedRobot class.

---
+++ src/main/java/frc/robot/AkLogger.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+// TODO-JYW: This class will extend the functionality of LoggedRobot
+// to add any custom logging features we want.
+import org.littletonrobotics.junction.LoggedRobot;
+
+public class AkLogger extends LoggedRobot {
+    
+}
