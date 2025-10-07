0971ed4d0592497792461bf2829bba65c728f9b9
James Y. Wilson <jywilson@ieee.org>
2025-10-06 19:51:55 -0700

    WIP: WARNING - WARNING: This commit is a work in progress and may not be stable.
    As shown in the code, additional changes are needed
    to recognize the invocation of a simulation mode.
    Also, integration testing is required to verify that
    the replacement of the TimedRobot base class with the
    LoggedRobot base class does not introduce any issues.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,13 @@ import com.pathplanner.lib.commands.PathfindingCommand;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.commands.ScoreCommands;
+
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 import org.photonvision.EstimatedRobotPose;
 
 import com.ctre.phoenix6.SignalLogger;
@@ -18,7 +25,6 @@ import com.ctre.phoenix6.Utils;
 
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.constants.Constants;
@@ -31,7 +37,7 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
     private Command autonomousCommand;
 
     private RobotContainer robotContainer;
@@ -41,6 +47,59 @@ public class Robot extends TimedRobot {
     private static AprilTagSubsystem aprilTagSubsystem = AprilTagSubsystem.getInstance();
 
 //  private IntakeSubsystem exampleSubsystem = IntakeSubsystem.getInstance();
+ 
+public Robot() {
+    // TODO: Support the use of Constants, such as those shown below for build metadata,
+    // and robot execution mode.
+
+    // // Record metadata
+    // Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+    // Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+    // Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+    // Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+    // Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+    // switch (BuildConstants.DIRTY) {
+    //   case 0:
+    //     Logger.recordMetadata("GitDirty", "All changes committed");
+    //     break;
+    //   case 1:
+    //     Logger.recordMetadata("GitDirty", "Uncomitted changes");
+    //     break;
+    //   default:
+    //     Logger.recordMetadata("GitDirty", "Unknown");
+    //     break;
+    // }
+
+    // // Set up data receivers & replay source
+    // switch (Constants.currentMode) {
+    //   case REAL:
+    //     // Running on a real robot, log to a USB stick ("/U/logs")
+    //     Logger.addDataReceiver(new WPILOGWriter());
+    //     Logger.addDataReceiver(new NT4Publisher());
+    //     break;
+
+    //   case SIM:
+    //     // Running a physics simulator, log to NT
+    //     Logger.addDataReceiver(new NT4Publisher());
+    //     break;
+
+    //   case REPLAY:
+    //     // Replaying a log, set up replay source
+    //     setUseTiming(false); // Run as fast as possible
+    //     String logPath = LogFileUtil.findReplayLog();
+    //     Logger.setReplaySource(new WPILOGReader(logPath));
+    //     Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+    //     break;
+    // }
+
+        Logger.addDataReceiver(new NT4Publisher());
+
+        // Start AdvantageKit logger
+        Logger.start();
+
+        // TESTING - TESTING: Delete this code when the above changes are finalized.
+        Logger.recordOutput("TESTING-TESTING", 0);
+    }
 
     /**
      * This function is run when the robot is first started up and should be used for any
